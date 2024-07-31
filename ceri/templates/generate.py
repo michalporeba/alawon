c16775ca0dc35010261c95efbde3bccd4e760ca4
Michał Poręba <michalporeba@gmail.com>
2024-07-31 12:45:27 +0100

    grouping and ordering of melodies

--- ceri/templates/generate.py
+++ ceri/templates/generate.py
@@ -2,13 +2,44 @@ import json
 import os
 import sys
 from jinja2 import Environment, FileSystemLoader
+from dataclasses import dataclass, field
 
+@dataclass
+class TunesSection:
+  name: str
+  tunes: list = field(default_factory=list)
 
-def render_all(tunes, template_name):
+def define_sections():
+  sections = [
+    TunesSection('march'),
+    TunesSection('hornpipe'),
+    TunesSection('reel'),
+    TunesSection('jig'),
+    TunesSection('other')
+  ]
+
+  return { s.name: s for s in sections }
+
+def sort_tunes(tunes):
+  return sorted(tunes, key=lambda t:t['title'])
+
+def group_tunes(tunes):
+  sections = define_sections()
+  for tune in tunes:
+    section = tune.get('rhythm',{}).get('name', 'other')
+    if section in sections.keys():
+      sections[section].tunes.append(tune)
+    else:
+      sections['other'].tunes.append(tune)
+      print(f'WARNING: [{tune["title"]}] is not categorised and is going to the "other" section')
+
+  return sections
+
+def render_all(sections, template_name):
   environment = Environment(loader=FileSystemLoader('jinja'))
   template = environment.get_template(template_name)
 
-  yield { "id": template_name, "output": template.render(tunes=tunes) }
+  yield { "id": template_name, "output": template.render(sections=sections) }
 
 
 def render_each(tunes, template_name):
@@ -31,6 +62,7 @@ def save(renders, path_template):
       file.write(render["output"])
 
 
+
 def get_tunes():
   def filter(tune):
     return (len(sys.argv) == 1 or tune.get('id') in sys.argv) and tune.get('hidden', "false") != "true"
@@ -48,15 +80,20 @@ def get_tunes():
   
 def main():
   tunes = get_tunes()
+
   save(render_each(tunes, 'tune.ly'), 'lytex/{id}.ly')
   save(render_each(tunes, 'tune-in-dots.lytex'), 'lytex/dots/{id}.lytex')
   save(render_each(tunes, 'tune-in-guitar.lytex'), 'lytex/guitar/{id}.lytex')
   save(render_each(tunes, 'tune-in-mandolin.lytex'), 'lytex/mandolin/{id}.lytex')
 
   if len(tunes) > 1:
-    save(render_all(tunes, 'book-in-dots.lytex'), 'lytex/book.dots.lytex')
-    save(render_all(tunes, 'book-in-guitar.lytex'), 'lytex/book.guitar.lytex')
-    save(render_all(tunes, 'book-in-mandolin.lytex'), 'lytex/book.mandolin.lytex')
+
+    tunes = sort_tunes(tunes)
+    sections = group_tunes(tunes).values()
+
+    save(render_all(sections, 'book-in-dots.lytex'), 'lytex/book.dots.lytex')
+    save(render_all(sections, 'book-in-guitar.lytex'), 'lytex/book.guitar.lytex')
+    save(render_all(sections, 'book-in-mandolin.lytex'), 'lytex/book.mandolin.lytex')
 
 
 if __name__ == '__main__':
